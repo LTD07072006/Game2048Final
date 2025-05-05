e2aa2a15422cd62934d38123d35ff6ad72977250
LTD07072006 <tuandung1232006@gmail.com>
2025-05-05 19:23:30 +0700

    Add files via upload

---
+++ 2048/x64/const.h
@@ -0,0 +1,10 @@
+#ifndef CONST_H
+#define CONST_H
+extern const int SCREEN_WIDTH;
+extern const int SCREEN_HEIGHT;
+extern const char* const WINDOW_TITLE;
+extern const int TILE_MARGIN;
+constexpr int ROWS = 4;
+constexpr int COLUMNS = 4;
+
+#endif // CONST_H
---
+++ 2048/x64/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include <SDL_mixer.h>
+#include "const.h"
+#include "logic.h"
+#include "graphics.h"
+#include "utils.h"
+
+SDL_Window* window = nullptr;
+SDL_Renderer* renderer = nullptr;
+SDL_Surface* surface = nullptr;
+TTF_Font* font = nullptr;
+Mix_Music* bgMusic = nullptr;
+SDL_Color FONT_COLOR = { 0, 0, 0, 255 };
+int main(int argc, char* argv[])
+{
+    init();
+    play_2048();
+    quitSDL(window, renderer, bgMusic);
+    return 0;
+}
